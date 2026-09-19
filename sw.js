2fff76f01566cf468d778da1b33733a2a5326be8
Максим Добычин <dobyvankenoby@gmail.com>
2026-09-19 23:04:27 +0300

    3D-дуэль: фигура центрируется по свободному месту и стала крупнее; иконка светло-фиолетовая (не как у Ночной смены)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Scoped offline application updater; version changes with the payload and updater.
-const VERSION = 'bd95736258';
+const VERSION = 'a8b1cc5e3c';
 const PREFIX = "emoplanet-";
 const CACHE = PREFIX + VERSION;
 const SCOPE = new URL(self.registration.scope);
